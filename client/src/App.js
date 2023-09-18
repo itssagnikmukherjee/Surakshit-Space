3c05a3d6683c525e797c005fefdfbd653f6bccfa
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2023-09-18 16:33:31 +0530

    feat--- share and files component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
+import Files from "./components/Files";
 import Navbar from "./components/Navbar";
+import Share from "./components/Share";
 import ContractContextProvider from "./context/ContractContextProvider";
 import Home from "./Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -9,6 +11,8 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/files" element={<Files />} />
+          <Route path="/share" element={<Share />} />
         </Routes>
       </BrowserRouter>
     </ContractContextProvider>
